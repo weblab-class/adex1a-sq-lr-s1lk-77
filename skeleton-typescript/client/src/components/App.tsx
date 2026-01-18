f23f7668021a7c362746bdf69aa78c74c48f9cdd
aadya <110504381+adex1a@users.noreply.github.com>
2026-01-17 19:34:36 -0800

    this still isnt working kms

--- skeleton-typescript/client/src/components/App.tsx
+++ skeleton-typescript/client/src/components/App.tsx
@@ -1,17 +1,8 @@
 import React, { useState, useEffect, createContext } from "react";
-import jwt_decode from "jwt-decode";
-import { CredentialResponse } from "@react-oauth/google";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { get, post } from "../utilities";
 
-import { socket } from "../client-socket";
 import { Outlet } from "react-router-dom";
-
-import NotFound from "./pages/NotFound";
-import WallView from "./pages/WallView";
-
-import User from "../../../shared/User";
 
 import Cat from "../../../shared/Cat";
 
@@ -28,38 +19,6 @@ export const ActiveCatContext = createContext<{
 });
 
 const App = () => {
-  const [userId, setUserId] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    get("/api/whoami")
-      .then((user: User) => {
-        if (user._id) {
-          // TRhey are registed in the database and currently logged in.
-          setUserId(user._id);
-        }
-      })
-      .then(() =>
-        socket.on("connect", () => {
-          post("/api/initsocket", { socketid: socket.id });
-        })
-      );
-  }, []);
-
-  const handleLogin = (credentialResponse: CredentialResponse) => {
-    const userToken = credentialResponse.credential;
-    const decodedCredential = jwt_decode(userToken as string) as { name: string; email: string };
-    console.log(`Logged in as ${decodedCredential.name}`);
-    post("/api/login", { token: userToken }).then((user) => {
-      setUserId(user._id);
-      post("/api/initsocket", { socketid: socket.id });
-    });
-  };
-
-  const handleLogout = () => {
-    setUserId(undefined);
-    post("/api/logout");
-  };
-
   const [activeCats, setActiveCats] = useState<Cat[]>([]);
   const getActiveCats = () => {
     get("/api/activecats").then((catData) => {
--- skeleton-typescript/client/src/index.tsx
+++ skeleton-typescript/client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, createContext } from "react";
 import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
@@ -10,11 +10,55 @@ import {
 import NotFound from "./components/pages/NotFound";
 import App from "./components/App";
 import WallView from "./components/pages/WallView";
+import Skeleton from "./components/pages/Skeleton";
+
+import jwt_decode from "jwt-decode";
+import { CredentialResponse } from "@react-oauth/google";
+import User from "../../shared/User";
+import { get, post } from "./utilities";
+import { socket } from "./client-socket";
+
+const [userId, setUserId] = useState<string | undefined>(undefined);
+
+useEffect(() => {
+  get("/api/whoami")
+    .then((user: User) => {
+      if (user._id) {
+        // TRhey are registed in the database and currently logged in.
+        setUserId(user._id);
+      }
+    })
+    .then(() =>
+      socket.on("connect", () => {
+        post("/api/initsocket", { socketid: socket.id });
+      })
+    );
+}, []);
+
+const handleLogin = (credentialResponse: CredentialResponse) => {
+  const userToken = credentialResponse.credential;
+  const decodedCredential = jwt_decode(userToken as string) as { name: string; email: string };
+  console.log(`Logged in as ${decodedCredential.name}`);
+  post("/api/login", { token: userToken }).then((user) => {
+    setUserId(user._id);
+    post("/api/initsocket", { socketid: socket.id });
+  });
+};
+
+const handleLogout = () => {
+  setUserId(undefined);
+  post("/api/logout");
+};
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route errorElement={<NotFound />} element={<App />}>
-      <Route path="/" element={<WallView />} />
+      <Route
+        element={<Skeleton handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />}
+        path="/"
+      />
+      <Route path="/wallview" element={<WallView />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
