ea61a70b371b4ac6c567227755824d1c951ac6cd
s1lk-77 <95396915+s1lk-77@users.noreply.github.com>
2026-01-26 12:50:17 -0500

    Merged speech bubble & catalog backbutton

--- client/src/components/pages/Catalog.tsx
+++ client/src/components/pages/Catalog.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { get } from "../../utilities";
 import Cat from "../../../../shared/Cat";
 import CatalogEntry from "../CatalogEntry";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import "./Settings.css";
 
 const Catalog = () => {
   const [allCats, setAllCats] = useState<Cat[]>([]);
@@ -15,12 +17,23 @@ const Catalog = () => {
     getAllCats();
   }, []);
 
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1> Cat - a - log</h1>
-      {allCats.map((cat) => (
-        <CatalogEntry cat={cat} />
-      ))}
+      <button className="settings-page__back" onClick={() => navigate(-1)}>
+        <img alt="go back" />
+      </button>
+
+      <h1 className="flex items-center justify-center p-sm" style={{ fontSize: `2em` }}>
+        {" "}
+        Cat - a - log
+      </h1>
+      <p className="flex flex-col items-center justify-center">
+        {allCats.map((cat) => (
+          <CatalogEntry cat={cat} key={cat.id} />
+        ))}
+      </p>
     </>
   );
 };
