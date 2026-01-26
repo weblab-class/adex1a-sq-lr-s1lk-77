dbed92f408dc46a4778e30b3c03c83c2bcee5b65
Luoxi Wu <xxxluoxi.wu.2007xxx@gmail.com>
2026-01-26 14:30:57 -0500

    responsive text bubbles. spoopy font o_O

--- client/src/components/cat_view/SpeechBubble.tsx
+++ client/src/components/cat_view/SpeechBubble.tsx
@@ -10,11 +10,26 @@ const SpeechBubble = (props: Props) => {
   return (
     <>
       {props.textcontent.length < 15 ? (
-        <p className="CatDisplay-speech" style={{ left: `45%`, fontSize: `2em` }}>
+        <p
+          className="CatDisplay-speech"
+          style={{
+            left: "50%",
+            transform: "translateX(-50%)",
+            fontSize: `clamp(1em, 2.5cqw, 1.8em)`,
+          }}
+        >
           {props.textcontent}
         </p>
       ) : (
-        <p className="CatDisplay-speech" style={{ left: `0%`, fontSize: `1.15em` }}>
+        <p
+          className="CatDisplay-speech"
+          style={{
+            left: "50%",
+            transform: "translateX(-50%)",
+            fontSize: `clamp(0.3em, 1cqw, 2em)`,
+            fontFamily: "Rubik Glitch",
+          }}
+        >
           {props.textcontent}
         </p>
       )}
